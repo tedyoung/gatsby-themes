91b1137bc3077da7693a6e30815f6c9c5d8a0554
arshad <arshad@chummun.com>
2020-01-22 09:34:26 +0400

    fix(flex): rename fill-rule to fillRule

--- themes/gatsby-theme-flex/src/layout/header.js
+++ themes/gatsby-theme-flex/src/layout/header.js
@@ -47,12 +47,12 @@ const Header = ({ logo, siteName, headerLinks }) => {
               >
                 {showMenu ? (
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M15.78 14.36a1 1 0 0 1-1.42 1.42l-2.82-2.83-2.83 2.83a1 1 0 1 1-1.42-1.42l2.83-2.82L7.3 8.7a1 1 0 0 1 1.42-1.42l2.83 2.83 2.82-2.83a1 1 0 0 1 1.42 1.42l-2.83 2.83 2.83 2.82z"
                   />
                 ) : (
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
                   />
                 )}
